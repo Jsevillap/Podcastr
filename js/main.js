4e6310920ec36e10cb904740292c966b3f4e07d9
Jesus Sevilla <jesus.sevilla.pena@gmail.com>
2021-09-02 20:21:25 -0500

    adjusted mobile view and added playtime of audio

--- js/main.js
+++ js/main.js
@@ -1,57 +1,120 @@
-const root = document.querySelector(":root");
-const themeSwitch = document.querySelector(".themeSwitch");
-let isDark = true;
+const
+    root = document.querySelector(":root"),
+    themeSwitch = document.querySelector(".themeSwitch"),
+    play = document.querySelector("#playButton"),
+    disc = document.querySelector(".spinDisc"),
+    currentPlay = document.querySelector("#currentPlayTime"),
+    timeLeft = document.querySelector("#playTimeLeft"),
+    audio = new Audio("./sound.mp3");
+
+
+let
+    isDark = true,
+    playing = false,
+    interval,
+    time = 0,
+    timeText = "";
+
+
+
+const lightMode = () => {
+    root.style.setProperty("--lighter-blue", "#f0f0f0");
+    root.style.setProperty("--dark-blue", "#fff");
+    root.style.setProperty("--primary-text-color", "#181b20");
+    root.style.setProperty("--variant-text-color", "#1e252d");
+    root.style.setProperty("--translate-pos", "translateX(23px)");
+    root.style.setProperty("--themeSwitch-text", "'Dark'");
+    root.style.setProperty("--shadow-value", " 0 0 13px 2px #1e252d3b");
+    isDark = false;
+};
+
+const darkMode = () => {
+    root.style.setProperty("--lighter-blue", "#1e252d");
+    root.style.setProperty("--dark-blue", "#181b20");
+    root.style.setProperty("--primary-text-color", "white");
+    root.style.setProperty("--variant-text-color", "#a9b0b8");
+    root.style.setProperty("--translate-pos", "translateX(0)");
+    root.style.setProperty("--themeSwitch-text", "'Light'");
+    root.style.setProperty("--shadow-value", " 0 0 0 0 #1e252d3b");
+    isDark = true;
+}
 
 const changeTheme = () => {
-    if (isDark) {
-        root.style.setProperty("--lighter-blue", "#f0f0f0");
-        root.style.setProperty("--dark-blue", "#fff");
-        root.style.setProperty("--primary-text-color", "#181b20");
-        root.style.setProperty("--variant-text-color", "#1e252d");
-        root.style.setProperty("--translate-pos", "translateX(23px)");
-        root.style.setProperty("--themeSwitch-text", "'Dark'");
-        root.style.setProperty("--shadow-value", " 0 0 13px 2px #1e252d3b");
-        isDark = false;
-    } else {
-        root.style.setProperty("--lighter-blue", "#1e252d");
-        root.style.setProperty("--dark-blue", "#181b20");
-        root.style.setProperty("--primary-text-color", "white");
-        root.style.setProperty("--variant-text-color", "#a9b0b8");
-        root.style.setProperty("--translate-pos", "translateX(0)");
-        root.style.setProperty("--themeSwitch-text", "'Light'");
-        root.style.setProperty("--shadow-value", " 0 0 0 0 #1e252d3b");
-        isDark = true;
-    }
+
+    isDark ? lightMode() : darkMode();
 
 }
 
+//Change the theme
 themeSwitch.addEventListener("click", changeTheme);
 
-const play = document.querySelector("#playButton");
-const disc = document.querySelector(".spinDisc");
-const currentPlay = document.querySelector("#currentPlayTime");
-const timeLeft = document.querySelector("#playTimeLeft");
-const audio = new Audio("./sound.mp3");
-let playing = false;
 
 
 
+const checkLength = () => {
 
-play.addEventListener("click", () => {
+    let audioLength = audio.duration;
+    let lengthTest = `0${Math.floor(audioLength / 60)}:${Math.floor(audioLength - Math.floor((audioLength / 60)) * 60)}`;
+    timeLeft.innerHTML = lengthTest;
+
+    return lengthTest;
+
+}
+
+const changeIconAndStartAnimation = () => {
     play.classList.toggle("lni-play");
     play.classList.toggle("lni-pause");
     disc.classList.toggle("spin");
+}
+
+
+play.addEventListener("click", () => {
+
+    changeIconAndStartAnimation();
+    checkLength();
+
+
 
     if (!playing) {
+
         audio.play();
         playing = true;
+        interval = setInterval(() => {
+
+            if (audio.ended) {
+                clearInterval(interval);
+                changeIconAndStartAnimation();
+                time = 0;
+                playing = false;
+                currentPlay.innerHTML = checkLength();
+                return
+            }
+            if (time === 206) { return } //return out of function if the end of the song is reached to avoid the text to display an extra second
+            time++;
+            if (time < 10) { currentPlay.innerHTML = "00:0" + time; }
+            else if (time >= 10 && time < 60) { currentPlay.innerHTML = "00:" + time; }
+            else if (time >= 60 && time - Math.floor((time / 60)) * 60 < 10) { currentPlay.innerHTML = `0${Math.floor(time / 60)}:0${time - Math.floor((time / 60)) * 60}` }
+            else if (time >= 60 && time - Math.floor((time / 60)) * 60 >= 10) { currentPlay.innerHTML = `0${Math.floor(time / 60)}:${time - Math.floor((time / 60)) * 60}` }
+
+
+
+
+        }, 1000)
+
+
+
     } else {
         audio.pause();
-        playing = false;;
+        playing = false;
+        clearInterval(interval);
     }
 
 
+
 });
+
+
+
 /*
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
